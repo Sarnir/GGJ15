a2d1c9a3c4a3660d6debf6949b4192e35a01d1f6
Edvardius <mtyszkiewicz@gmail.com>
2015-01-23 22:52:28 +0100

    duzo

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -3,17 +3,27 @@ using System.Collections;
 
 public class LevelController : MonoBehaviour
 {
-	public GameObject obiekt;
+	public new Camera camera;
 
 	void Start ()
 	{
+		if (Application.loadedLevel == 0)
+		{
+
+		}
+		else if (Application.loadedLevel == 1)
+		{
+		}
+		else if (Application.loadedLevel == 2)
+		{
+		}
 	}
 	
 	void Update ()
 	{
 		if (Input.GetKey(KeyCode.Return))
 		{
-			Application.LoadLevel("level2");
+			Application.LoadLevel(Application.loadedLevel + 1);
 		}
 	}
 }
