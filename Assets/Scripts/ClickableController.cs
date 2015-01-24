8a359191de2a9bc6f41087475d021dade5b5f33e
Edvardius <mtyszkiewicz@gmail.com>
2015-01-24 15:41:49 +0100

    lots of sheyt

---
+++ Assets/Scripts/ClickableController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickableController : MonoBehaviour
+{
+	public Sprite button_on;
+	public Sprite button_off;
+	private SpriteRenderer sr;
+
+	void Start ()
+	{
+		sr = GetComponent<SpriteRenderer>();
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	void OnMouseDown()
+	{
+		sr.sprite = button_on;
+	}
+
+	void OnMouseUp()
+	{
+		sr.sprite = button_off;
+	}
+}
