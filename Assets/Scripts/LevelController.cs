745339bd7cb4d5324e47b856ac1c95440a005b5f
piwo-ps <piotr.wolski@playsoft.fr>
2015-01-24 12:47:00 +0100

    SLOW MOTION

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class LevelController : MonoBehaviour
 {
+	const float slowmoScale = 0.1f;
+	const float normalScale = 2.0f;
+	const float shiftSpeed = 2.0f;
+	bool slowDown;
+	float lerpProgress;
+
 	void Start ()
 	{
+		Time.timeScale = normalScale;
+		lerpProgress = 1.0f;
+		slowDown = false;
 	}
 	
 	void Update ()
@@ -13,5 +22,19 @@ public class LevelController : MonoBehaviour
 		{
 			Application.LoadLevel(Application.loadedLevel + 1);
 		}
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			slowDown = !slowDown;
+		}
+
+		if(slowDown)
+			lerpProgress -= Time.deltaTime * shiftSpeed;
+		else
+			lerpProgress += Time.deltaTime * shiftSpeed;
+
+		lerpProgress = Mathf.Clamp(lerpProgress, 0.0f, 1.0f);
+
+		Time.timeScale = Mathf.Lerp(slowmoScale, normalScale, lerpProgress);
     }
 }
