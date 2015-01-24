40090ae266b8fd2e7fa200747025200ed6ee1d33
Edvardius <mtyszkiewicz@gmail.com>
2015-01-24 14:31:01 +0100

    sporo

---
+++ Assets/IgnoreFirstCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgnoreFirstCollision : MonoBehaviour
+{
+	private int ignoreLayer = 8;
+	private int defaultLayer = 0;
+
+	void Start()
+	{
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (gameObject.layer == ignoreLayer && collision.gameObject.tag == "cat")
+		{
+			gameObject.layer = defaultLayer;
+		}
+	}
+}
